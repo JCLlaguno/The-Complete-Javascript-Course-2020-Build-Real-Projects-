05353d63b134998dc5920ef6700db73e3e2c7e5c
JC Llaguno 21 <johnchristopherllaguno@gmail.com>
2020-10-06 19:58:42 +0800

    updating the budget - ui controller

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -172,7 +172,11 @@ var UIController = (function() {
         inputValue: '.add__value',
         inputBtn: '.add__btn',
         incomeContainer: '.income__list',
-        expensesContainer: '.expenses__list'
+        expensesContainer: '.expenses__list',
+        budgetLabel: '.budget__value',
+        incomeLabel: '.budget__income--value',
+        expenseLabel: '.budget__expenses--value',
+        percentageLabel: '.budget__expenses--percentage'
         
     }
     
@@ -243,6 +247,24 @@ var UIController = (function() {
             
         },
         
+        displayBudget: function(obj) {
+            
+            document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+            document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+            document.querySelector(DOMstrings.expenseLabel).textContent = obj.totalExp;
+            
+            if(obj.percentage > 0) {
+                
+                document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';    
+                
+            } else {
+                
+                document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+                
+            }
+            
+        },
+        
         // return the dom string elements (classes)
         getDOMstrings: function() {
         
@@ -285,7 +307,7 @@ var controller = (function(budgetCtrl, UICtrl) {
         var budget = budgetCtrl.getBudget();
         
         // 3. Display the budget on the UI
-        console.log(budget);
+        UICtrl.displayBudget(budget);
         
     }
     
@@ -319,6 +341,14 @@ var controller = (function(budgetCtrl, UICtrl) {
         init: function() {
         
             console.log('Application has started');
+            UICtrl.displayBudget({
+                
+                budget: 0,
+                totalInc: 0,
+                totalExp: 0,
+                percentage: -1 
+                
+            });
             setupEventListeners();
 
         }

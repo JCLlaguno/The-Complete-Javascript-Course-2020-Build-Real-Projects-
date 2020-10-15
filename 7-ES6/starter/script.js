2da34ad79bba572d437b4c7978e6bd3d0a0b3796
JC Llaguno 21 <johnchristopherllaguno@gmail.com>
2020-10-15 21:00:04 +0800

    arrow functions - lexical 'this' keyword

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -158,6 +158,7 @@ there are three ways of writing arrow functions:
     
 */
 
+/*
 const years = [1990, 1965, 1982, 1937];
 
 // ES5
@@ -186,9 +187,9 @@ console.log(ages6);
 
 
 // more than one argument and line of code
-/* 
-if the return part of the arrow function has more than one line of code (ended by ';'), you need to use curly braces '{}' and a return keyword
-*/
+
+// if the return part of the arrow function has more than one line of code (ended by ';'), you need to use curly braces '{}' and a return keyword
+
 ages6 = years.map((el, index) => {
     
     const now = new Date().getFullYear(); // 1 line
@@ -198,6 +199,115 @@ ages6 = years.map((el, index) => {
 });
 
 console.log(ages6);
+*/
+
+
+// Lecture: Arrow functions 2
+// ES5
+// using regular callback function
+var box5 = {
+    color: 'green',
+    position: 1,
+    
+    // clickMe is a method attached to an object so, 'this' points to box5
+    clickMe: function() {
+        
+        var self = this; // this is used to point 'this' to box5 (object) and not the window object (this enables access to the color and position property)
+        
+        document.querySelector('.green').addEventListener('click', function() {
+            
+            var str = 'This is box number ' + self.position + ' and it is ' + self.color;
+            alert(str);
+            
+        });
+    }
+}
+
+//box5.clickMe();
+
+
+// ES6
+// using an arrow function shares the 'this' keyword (lexical this) from the object 'box6'
+// in ES6, use arrow functions if you want to preserve the value of the 'this' keyword
+const box6 = {
+    color: 'green',
+    position: 1,
+    clickMe: function() {
+        
+        // when using arrow functions, use parentheses '()' when there is no parameter or more than one parameter
+        document.querySelector('.green').addEventListener('click', () => {
+            
+            var str = 'This is box number ' + this.position + ' and it is ' + this.color;
+            alert(str);
+            
+        });
+    }
+}
+
+//box6.clickMe();
+
+
+// ES6
+// using arrow function on a method of an object
+//const box66 = {
+//    color: 'green',
+//    position: 1,
+//    
+//    // using an arrow function here points the 'this' to the window object and not the box66 object
+//    // will result in undefined for 'position' and 'color' because both properties are not present in the window object
+//    clickMe: () => {
+//        
+//        document.querySelector('.green').addEventListener('click', () => {
+//            
+//            var str = 'This is box number ' + this.position + ' and it is ' + this.color;
+//            alert(str);
+//            
+//        });
+//    }
+//}
+//
+//box66.clickMe();
+
+
+// example using a function constructor
+function Person(name) {
+    
+    this.name = name;
+    
+}
+
+// ES5 example using callback and bind method
+// the objects created through the Person function constructor inherit this method
+Person.prototype.myFriends5 = function(friends) {
+    
+    var arr = friends.map(function(el) {
+        
+        return this.name + ' is friends with ' + el;
+        
+    }.bind(this)); // bind() creates a copy of the function with the 'this' variable set to 'this' (Person), replacing the window object
+    
+    console.log(arr);
+    
+};
+
+var friends = ['Bob', 'Jane', 'Mark'];
+new Person('John').myFriends5(friends);
+
+
+// ES6
+// example using arrow function and template string
+Person.prototype.myFriends6 = function(friends) {
+    
+    // this arrow function shares the lexical 'this' from the surroundings, in this case the myFriends6 function, which contains the 'this' that points to the instance of the Person (Mike)
+    var arr = friends.map(el => `${this.name} is friends with ${el}`);
+    
+    console.log(arr);
+    
+};
+
+var friends = ['Bob', 'Jane', 'Mark'];
+new Person('Mike').myFriends6(friends);
+
 
 
 
